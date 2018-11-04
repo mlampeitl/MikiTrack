9866b4f442424bac72a0694da04e0898e5d7f0d5
Michael Lampeitl <mika@mikadesk.home>
2018-11-04 15:51:27 +0100

    Version 0.0.1

---
+++ MikiTrack/GpxParser.swift
@@ -0,0 +1,9 @@
+//
+//  GpxParser.swift
+//  MikiTrack
+//
+//  Created by Michael Lampeitl on 04.11.18.
+//  Copyright © 2018 AlienTec. All rights reserved.
+//
+
+import Foundation
---
+++ MikiTrack/GpxParserError.swift
@@ -0,0 +1,9 @@
+//
+//  GpxParserError.swift
+//  MikiTrack
+//
+//  Created by Michael Lampeitl on 04.11.18.
+//  Copyright © 2018 AlienTec. All rights reserved.
+//
+
+import Foundation
