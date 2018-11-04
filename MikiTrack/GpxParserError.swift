fa0c5ae90d8a2844ba85d6718396a74d01b65110
Michael Lampeitl <mika@mikadesk.home>
2018-11-04 16:09:21 +0100

    restart

--- MikiTrack/GpxParserError.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GpxParserError.swift
-//  MikiTrack
-//
-//  Created by Michael Lampeitl on 04.11.18.
-//  Copyright © 2018 AlienTec. All rights reserved.
-//
-
-import Foundation
--- MikiTrack/GpxTrackPoint.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GpxTrackPoint.swift
-//  MikiTrack
-//
-//  Created by Michael Lampeitl on 04.11.18.
-//  Copyright © 2018 AlienTec. All rights reserved.
-//
-
-import Foundation
