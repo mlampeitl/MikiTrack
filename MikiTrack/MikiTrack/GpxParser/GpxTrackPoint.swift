309f4df8c85b276af0b6e702206dc3616accad1a
Michael Lampeitl <michael@lampeitl.net>
2018-11-06 00:49:14 +0100

    Lattitude, Longitude, Elevation, Timestamp are now Properties Calculate Hash started

--- MikiTrack/MikiTrack/GpxParser/GpxTrackPoint.swift
+++ MikiTrack/MikiTrack/GpxParser/GpxTrackPoint.swift
@@ -10,26 +10,65 @@ import Foundation
 
 class GpxTrackPoint
 {
-    var lattitude : Double?
-    var longitude : Double?
-    var elevation : Double?
-    var timestamp : Date?
+    private var _hash : Data?
+    private var _lattitude : Double?
+    private var _longitude : Double?
+    private var _elevation : Double?
+    private var _timestamp : Date?
     
     init ()
     {
-        lattitude = nil;
-        longitude = nil;
-        elevation = nil;
-        timestamp = nil;
+        _hash = nil;
+        _lattitude = nil;
+        _longitude = nil;
+        _elevation = nil;
+        _timestamp = nil;
     }
     
     convenience init(ts: Date, lat: Double, lon: Double, ele: Double? = nil)
     {
         self.init();
-        lattitude = lat;
-        longitude = lon;
-        elevation = ele;
-        timestamp = ts;
+        _lattitude = lat;
+        _longitude = lon;
+        _elevation = ele;
+        _timestamp = ts;
     }
 
+    ///
+    /// Properties
+    ///
+    var lattitude : Double?
+    {
+        get { return _lattitude; }
+        set { _lattitude = newValue; }
+    }
+    var longitude : Double?
+    {
+        get { return _longitude; }
+        set { _longitude = newValue; }
+    }
+    var elevation : Double?
+    {
+        get { return _elevation; }
+        set { _elevation = newValue; }
+    }
+    var timestamp : Date?
+    {
+        get { return _timestamp; }
+        set { _timestamp = newValue; }
+    }
+    
+    //Calculate hash
+    private func calculateHash()
+    {
+        var myLatStr = "";
+        var myLonStr = "";
+        var myTsStr = "";
+        if _lattitude != nil { myLatStr = String(_lattitude!); }
+        if _longitude != nil { myLonStr = String(_longitude!); }
+        if _timestamp != nil { myTsStr = ISO8601DateFormatter().string(from: _timestamp!); }
+        let myString = String("\(myLatStr)|\(myLonStr)|\(myTsStr)");
+        
+        //ToDo: Implement CryptoSwift
+    }
 }
